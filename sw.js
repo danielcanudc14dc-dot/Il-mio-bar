7ab9fa90c1b757b0913ef8882ee54dc55a0d270d
danielcanudc14dc-dot <daniel.canu.dc14.dc@gmail.com>
2026-09-19 02:12:38 +0200

    Add files via upload
    Aggiornamento multiclienti

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='il-mio-bar-v2';
+const FILES=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('il-mio-bar-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
