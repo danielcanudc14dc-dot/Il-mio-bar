c577f949ec5128352d9d0829312829b04640f582
danielcanudc14dc-dot <daniel.canu.dc14.dc@gmail.com>
2026-09-21 17:40:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='il-mio-bar-v2';
+const CACHE='il-mio-bar-v3';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('il-mio-bar-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
